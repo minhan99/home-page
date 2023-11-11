d26b43da8a93bb224358671fb84b50ba2773395a
minhan99 <sitdown197@gmail.com>
2023-11-11 23:49:21 +0700

    upcode header p1

--- src/components/BackGroundHeader/BackGroundHeader.jsx
+++ src/components/BackGroundHeader/BackGroundHeader.jsx
@@ -1,10 +1,17 @@
-import { Box } from '@mui/material'
+import { Box, CardMedia } from '@mui/material'
 import React from 'react'
+import Header from '../Header/Header'
 
 function BackGroundHeader() {
   return (
     <Box>
-        <h1>Đây là trang Header của Home</h1>
+        <CardMedia src='https://www.youtube.com/embed/JPe2mwq96cw?autohide=1&rel=0&autoplay=0&playsinline=1&iv_load_policy=3&modestbranding=1&controls=0&showinfo=0&disablekb=1&enablejsapi=1&origin=https%3A%2F%2Fthemes.themegoods.com&widgetid=1'></CardMedia>
+        {/* Thanh menu header */}
+        <Box>
+          <Header></Header>
+        </Box>
+        {/* Khung search tìm kiếm trên header */}
+        <Box></Box>
     </Box>
   )
 }
